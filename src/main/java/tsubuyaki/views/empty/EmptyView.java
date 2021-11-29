fd3600a7a750b278826cf70a771c00cb5a03450d
start.vaadin.com <no-reply@vaadin.com>
2021-11-29 08:13:26 +0000

    Generated project

---
+++ src/main/java/tsubuyaki/views/empty/EmptyView.java
@@ -0,0 +1,32 @@
+package tsubuyaki.views.empty;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Empty")
+@Route(value = "empty")
+@RouteAlias(value = "")
+public class EmptyView extends VerticalLayout {
+
+    public EmptyView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
